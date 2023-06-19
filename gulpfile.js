9ebbdae88cadee3460c68b86099093998db0ab48
Maksym Siroshtan <106271805+Maksym-Siroshtan@users.noreply.github.com>
2023-06-19 05:13:19 +0300

    Changed adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
-//const dist = "/OSPanel/domains/windowServer";
+//const dist = "/OSPanel/domains/server/window";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
